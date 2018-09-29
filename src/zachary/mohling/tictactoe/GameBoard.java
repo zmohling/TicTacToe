35a94a2a10715152ebdfc1c8b08e4b019615ffce
Zachary Mohling <zmohling@gmail.com>
2018-09-29 01:46:28 -0500

    Final commit

--- src/zachary/mohling/tictactoe/GameBoard.java
+++ src/zachary/mohling/tictactoe/GameBoard.java
@@ -27,6 +27,8 @@ public class GameBoard extends JFrame {
 	 */
 	public boolean isInstantiated = false;
 	
+	private int[] initialGameBoardDimensions = new int[2];
+	
 	/**
 	 * Creates a Tic-Tac-Toe game board with predetermined dimensions and properties.
 	 */
@@ -45,6 +47,10 @@ public class GameBoard extends JFrame {
 		this.setSize((int)(screenSize.height * 0.5), (int)(screenSize.height * 0.5));		// Set GameBoard size to a square with lengths as long as screen height * 0.95
 		Dimension maximumSize = new Dimension(screenSize.height, screenSize.height);
 		this.setMaximumSize(maximumSize);
+		
+		initialGameBoardDimensions[0] = this.getWidth();
+		initialGameBoardDimensions[1] = this.getHeight();
+		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		
@@ -69,11 +75,16 @@ public class GameBoard extends JFrame {
 			public void componentResized(ComponentEvent e)
 			{
 				Rectangle window = e.getComponent().getBounds();
-				e.getComponent().setBounds(window.x, window.y, window.width, window.width);		// Maintain aspect ratio of 1x1
+				
+				/* Set bounds to the axis with the larger difference */
+				e.getComponent().setBounds(window.x, window.y, returnLargerDifference(initialGameBoardDimensions[0], window.width, initialGameBoardDimensions[1], window.height), returnLargerDifference(initialGameBoardDimensions[0], window.width, initialGameBoardDimensions[1], window.height));		// Maintain aspect ratio of 1x1
 				
 				/* Reinstantiate bounds if size is greater than maximum */
 				if (e.getComponent().getSize().getWidth() > maximumSize.getWidth())
 					e.getComponent().setSize(maximumSize);
+				
+				initialGameBoardDimensions[0] = e.getComponent().getWidth();
+				initialGameBoardDimensions[1] = e.getComponent().getHeight();
 				
 				/* If grid has been instantiated prior to GameBoard's resize, reflect those changes onto the components within grid */
 				if (isInstantiated)
@@ -107,6 +118,26 @@ public class GameBoard extends JFrame {
 		});
 				
 		this.setVisible(true);		// Set Frame Visible
+	}
+	
+	/**
+	 * Returns the integer with the larger difference between its current value and its initial value.
+	 * @param initial_first initial value of first integer argument
+	 * @param current_first	current value of first integer argument
+	 * @param initial_second initial value of second integer argument
+	 * @param current_second current value of second integer argument
+	 * @return integer with the larger difference between its current value and its initial value
+	 */
+	private int returnLargerDifference(int initial_first, int current_first, int initial_second, int current_second)
+	{
+		int firstDifference = Math.abs(current_first - initial_first);
+		int secondDifference = Math.abs(current_second - initial_second);
+		
+		if (firstDifference > secondDifference)
+			return current_first;
+		else
+			return current_second;
+			
 	}
 	
 	/**
